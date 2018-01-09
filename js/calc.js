4e51672b329acf8cdfa9b2879a35f0f1e3f72e29
Mubarak Okunade <mubarak.okunade@gmail.com>
2018-01-09 09:52:16 +0100

    2nd commit:Good progress :)

---
+++ js/calc.js
@@ -0,0 +1,29 @@
+"use strict";
+const pF = 0.8;
+const backupTime = 12;
+
+function sizingCalculator() {
+    var load = document.getElementById("load").value;
+    var panelRating = document.getElementById("pRating").value;
+    var battVolt = document.getElementById("batteryVoltage").value;
+    var maxLoad = load*1.5;
+    var solarPanelOutput = maxLoad*2;
+    var noOfPanels = Math.round(solarPanelOutput/panelRating);
+    var realPower = maxLoad;
+    var apparentPower = realPower/pF;
+    var battVoltage = battVolt;
+    var AH_rating = Math.round((maxLoad*backupTime*2)/battVoltage);
+    document.getElementById("load").innerHTML = "Load: "+load+" W\n";
+    document.getElementById("maxLoad").innerHTML = "Max Load: "+maxLoad+" W\n";
+    document.getElementById("npanels").innerHTML = "Number of Panels needed: "+noOfPanels+"\n";
+    document.getElementById("invrating").innerHTML = "Inverter rating: "+apparentPower+" VA\n";
+    document.getElementById("battvolt").innerHTML = "Battery Voltage: "+battVoltage+" V\n";
+    document.getElementById("totalahreq").innerHTML = "Total AH required: "+AH_rating+" AH\n";
+    console.log("Load: "+load+" W\n");
+    console.log("Max Load: "+maxLoad+" W\n");
+    console.log("Number of Panels needed: "+noOfPanels+"\n");
+    console.log("Inverter rating: "+apparentPower+" VA\n");
+    console.log("Battery Voltage: "+battVoltage+" V\n");
+    console.log("Total AH required: "+AH_rating+" AH\n");
+}
+
